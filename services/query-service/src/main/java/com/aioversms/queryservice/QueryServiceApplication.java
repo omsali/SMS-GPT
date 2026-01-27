7c0549918159f51b4824e1004282ac7d39c0587a
Om Sali <omsali.2810@gmail.com>
2026-01-27 13:12:55 -0600

    feat: bootstrap spring boot app with postgres config

---
+++ services/query-service/src/main/java/com/aioversms/queryservice/QueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aioversms.queryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryServiceApplication.class, args);
+	}
+
+}
